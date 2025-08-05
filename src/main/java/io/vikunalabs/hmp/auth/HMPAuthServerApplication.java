bf6c6d210d3a5776939cbd911ff345af5d77b0a1
vijaykumarn <vijaykumarn>
2025-08-05 15:49:32 +0200

    feat(HMP-1001): add registration workflow

---
+++ src/main/java/io/vikunalabs/hmp/auth/HMPAuthServerApplication.java
@@ -0,0 +1,13 @@
+package io.vikunalabs.hmp.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMPAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HMPAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/vikunalabs/hmp/auth/HMPAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vikunalabs.hmp.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HMPAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
